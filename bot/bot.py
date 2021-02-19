74312281687fe48de56d7a87d19e5e45a722fc68
instagr4h4m <gh1315@nyu.edu>
2021-02-19 10:52:56 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,17 +2,18 @@ import tweepy
 import random
 import cards
 
-
+# Keys needed to connect to Twitter API
 CONSUMER_KEY = '44X3mgoSsAinnLVA6IOTl4n3e'
 CONSUMER_SECRET = 'V4F99ZGZibM5hd1qp5GsyjesVkLJzeYzyD3nuK7Tmads1iBd8q'
 ACCESS_KEY = '1358837739098165249-n5rb14wYhHT30i8DXA80IqyMdJq2lh'
 ACCESS_SECRET = 'P5MK3LjvCn00XpVeu1v0Y3tcyJG3tyqVL7qM56K8iyLiz'
 
+# Use Tweepy to tweet
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-# Random variables
+# Randomize the cards (if it's a suit card)
 num_pos = random.randint(0,13)
 suit_pos = random.randint(0,3)
 
